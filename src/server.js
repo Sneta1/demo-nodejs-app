d3427abf8bd7c7944f027b589ecb43934cb7c48a
Sneta1 <45085960+Sneta1@users.noreply.github.com>
2023-05-02 21:15:19 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello there! Thanks for stopping by");
+    response.end("Hello there! Welcome again");
 
 });
 
